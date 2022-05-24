1c4fa4b8a6cdb7c30fe3106b482fb42a124fd39b
pkiraly <pkiraly@gwdg.de>
2022-05-24 14:55:20 +0200

    Mean FAIR values

--- classes/Fair.php
+++ classes/Fair.php
@@ -7,6 +7,24 @@ class Fair extends BaseTab {
     $frequency = $this->db->fetchAssocList($this->db->getFrequency($this->schema, $this->provider_id, $this->set_id), 'field');
     $smarty->assign('frequency', $frequency);
     $smarty->assign('variability', $this->db->fetchAssocList($this->db->getVariablitily($this->schema, $this->provider_id, $this->set_id), 'field'));
+
+    $means = [];
+    foreach ($frequency as $key => $values) {
+      if (preg_match('/^(Q.*):score$/', $key, $matches)) {
+        $Q = $matches[1];
+        $count = 0;
+        $total = 0;
+        foreach ($values as $record) {
+          if (!is_null($record['value']) && $record['value'] != 'NA') {
+            $count += $record['frequency'];
+            $total += ($record['frequency'] * $record['value']);
+          }
+        }
+        $means[$Q] = ($count > 0) ? sprintf("%.2f", ($total / $count)) : '&mdash;';
+      }
+    }
+    $smarty->assign('means', $means);
+
     $count = 0;
     $total = 0;
     $not_measured = 0;
